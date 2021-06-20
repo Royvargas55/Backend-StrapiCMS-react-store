4f530096e81afd28b2825d4be7b476d841a018e2
Royvargas55 <royvargas55@gmail.com>
2021-06-19 19:43:22 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec7ac5a3401fa13bb172a535f1aeb595'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3016e915-6dbb-4b02-ad9a-2bbb91c80079'
+};
